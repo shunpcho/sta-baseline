25f26192e175145362f0bee66367ef3e1da3af14
shunpcho <kshuchi0203@gmail.com>
2026-08-07 01:11:57 +0900

    feat: Add typealias

--- src/sta_baseline/utils/type_alias.py
+++ src/sta_baseline/utils/type_alias.py
@@ -10,6 +10,26 @@ class Split(Enum):
     TEST = auto()
 
 
+class FHOSTA(TypedDict):
+    info: FHOSTAInfo
+    annotations: list[FHOSTAAnnotation]
+
+
+class FHOSTAInfo(TypedDict):
+    video_metadata: dict[str, FHOVideoMetadata]  # The keys are video ids.
+    year: int
+    date_created: str  # format: YYYY-MM-DD
+
+
+type FHOVideoMetadata = dict[str, FrameInfo]  # The keys are video ids.
+
+
+class FrameInfo(TypedDict):
+    frame_width: int
+    frame_height: int
+    fps: float
+
+
 class FHOSTAAnnotation(TypedDict):
     uid: str
     main_uid: str
@@ -41,3 +61,12 @@ class ObjectsAnnotation(TypedDict):
     verb_category: int
     noun_category: int
     time_to_contact: float
+
+
+type ObjectDetectionAnnotation = dict[str, list[ObjectBoxes]]
+
+
+class ObjectBoxes(TypedDict):
+    box: list[float]
+    score: float
+    noun_category_id: int
